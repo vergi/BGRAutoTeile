a98c134f79ed1a7abe935d0de3323e018b6efd85
vergi <vergil@applet-bg.com>
2014-10-10 15:36:09 +0200

    Site

---
+++ js/resp-nav.js
@@ -0,0 +1,13 @@
+var nav = responsiveNav(".nav-collapse", { // Selector
+	animate: true, // Boolean: Use CSS3 transitions, true or false
+	transition: 284, // Integer: Speed of the transition, in milliseconds
+	label: "Menu", // String: Label for the navigation toggle
+	insert: "before", // String: Insert the toggle before or after the navigation
+	openPos: "relative", // String: Position of the opened nav, relative or static
+	navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
+	navActiveClass: "js-nav-active", // String: Class that is added to  element when nav is active
+	jsClass: "js", // String: 'JS enabled' class which is added to  element
+	init: function(){}, // Function: Init callback
+	open: function(){}, // Function: Open callback
+	close: function(){} // Function: Close callback
+});
